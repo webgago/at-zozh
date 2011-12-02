691bd595f832827964e4dde11d1d24600afe69f0
Anton Sozontov <asozontov@at-consulting.ru>
2011-12-02 12:23:14 +0400

    ЗОЖ init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtZozh::Application.config.secret_token = 'bd645273b486612632f55aac2b3a93c1193334d69c702156fc1420ef5b7f59283c8b045c38453ddba8e28833d14d31d1c8471b0a6522f0295ebc0e180706afce'
